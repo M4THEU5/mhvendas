71815cd01060f482df2287c90bd6e108529732d3
Matheus Henrique <matheusbynickzinho@hotmail.com>
2018-12-30 14:16:37 -0300

    Create model and controllers

---
+++ backend/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class ProdutoController extends Controller
+{
+
+    public function show($id)
+    {
+        //
+    }
+}
---
+++ backend/app/Http/Controllers/VendaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class VendaController extends Controller
+{
+
+    public function show($id)
+    {
+        //
+    }
+}
